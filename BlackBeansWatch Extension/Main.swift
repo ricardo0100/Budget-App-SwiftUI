db1021a1a75165f182eb784a008e768ec1211fed
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2020-06-28 09:36:15 -0300

    Migrate watch target to Xcode 12 beta

---
+++ BlackBeansWatch Extension/Main.swift
@@ -0,0 +1,19 @@
+//
+//  Main.swift
+//  BlackBeansWatch Extension
+//
+//  Created by Ricardo Gehrke on 28/06/20.
+//  Copyright © 2020 Ricardo Gehrke Filho. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Main: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
